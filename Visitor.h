eebe2125ab496d60c41d5b03b922f6d6d803307f
gizellemohamed <gizellemohamed@gmail.com>
2025-11-12 15:29:53 +0200

    Implement visitor class with member functions
    Added visitor class with constructors, destructor, and methods for name and ticket management.

--- Visitor.h
+++ Visitor.h
@@ -1,12 +1,20 @@
 #ifndef VISITOR_H
 #define VISITOR_H
-
-#include <string>
-using namespace std;
-
-//define visitor class here
-
-
+#include<iostream>
+using namespace std ;
+class visitor {
+    private:
+    string visitorName;
+    int ticketsBought;
+    public:
+    visitor();
+    visitor(string visitorName, int ticketsBought);
+    ~visitor();
+    void getvisitorName ();
+    void getticketsBought();
+    string setvisitorName(string v);
+    int setticketsBought(int t);
+};
 
 
 
