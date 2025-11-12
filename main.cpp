cc91bde19bd0ffaab0eba8a08459bb962bd50bb9
gizellemohamed <gizellemohamed@gmail.com>
2025-11-12 15:35:19 +0200

    Refactor main.cpp and implement animal class
    Refactor includes and implement animal class methods

--- main.cpp
+++ main.cpp
@@ -1,16 +1,63 @@
-#include "Animal.h"
-#include "Bird.h"
-#include "Mammal.h"
-#include "Reptile.h"
-#include "Enclosure.h"
-#include "Visitor.h"
-#include <iostream>
-using namespace std;
-
-
-// ============== MAIN FUNCTION ==============
-int main() {
-    
-    
-    return 0;
+#include<iostream>
+#include "animal.h"
+#include "enclosure.h"
+#include"visitor.h"
+#include "animal.h"
+#include "bird.h"
+#include "mammal.h"
+#include "reptile.h"
+using namespace std ;
+animal::animal(){
+     age=0;
+     name="";
+      bool isHungry=false;
 }
+animal::animal(string name,int age){
+    this->name=name;
+     this->age=age;
+    
+}
+    string animal::getname(){
+        return name;
+    }
+void animal::setname(string n){
+    name=n;
+}
+int animal::getage(){
+    return age;
+}
+  void animal::setage(int a){
+        age=a;
+    }
+   animal:: ~ animal(){
+
+    }
+    void animal::display(){
+cout<<name<<endl;
+cout<<age<<endl;
+    }
+    void animal::feed(){
+        
+    
+        }
+
+   enclosure::enclosure():animal(){
+    animals=new animal[];
+    currentcount=0;
+    capacity=0;
+   }
+int enclosure::getcurentcount(){
+    return currentcount;
+}
+string enclosure::getname(){
+    return name;
+}
+int  enclosure::getcapacity(){
+    return capacity;
+}
+void enclosure::setcurrentcount(int c){
+    cuurentcount=c;
+}
+
+int main(){
+    return 0;}
