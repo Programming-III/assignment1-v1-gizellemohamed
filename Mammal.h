8c010e7410dfa23fbca120368b0b2c9bd45dccea
gizellemohamed <gizellemohamed@gmail.com>
2025-11-12 15:28:16 +0200

    Define mammal class and include necessary headers
    Added includes and defined the mammal class with furColor.

--- Mammal.h
+++ Mammal.h
@@ -1,11 +1,14 @@
 #ifndef MAMMAL_H
 #define MAMMAL_H
-
-#include "Animal.h"
-
-//define mammal class here
-
-
+#include<iostream>
+#include "animal.h"
+using namespace std ;
+class mammal:public animal {
+string furColor;
+public:
+mammal();
+};
+#endif
 
 
 
